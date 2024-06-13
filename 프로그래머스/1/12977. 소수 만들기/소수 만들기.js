c538b64aa991d94b5fe5fa4503efa7763a5ae95b
Yonghyun Kim <81367886+yonghyun421@users.noreply.github.com>
2024-06-13 18:46:34 +0900

    [level 1] Title: 소수 만들기, Time: 0.07 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/12977. 소수 만들기/소수 만들기.js
@@ -0,0 +1,24 @@
+function solution(nums) {
+    let count = 0;
+
+    const isPrime = (num)  => {
+    if (num < 2) return false;
+    for (let i = 2; i <= Math.sqrt(num); i++) {
+        if (num % i === 0) return false;
+    }
+    return true;
+}
+
+    for (let i = 0; i < nums.length - 2; i++) {
+        for (let j = i + 1; j < nums.length - 1; j++) {
+            for (let k = j + 1; k < nums.length; k++) {
+                const sum = nums[i] + nums[j] + nums[k];
+                if (isPrime(sum)) {
+                    count++;
+                }
+            }
+        }
+    }
+
+    return count;
+}
